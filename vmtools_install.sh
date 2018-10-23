42915aa912eecd49e557f66f0b61b7e3f18031e0
movvamrocks <movvam@rocketmail.com>
2018-10-23 11:51:57 -0700

    Update vmtools_install.sh

--- vmtools_install.sh
+++ vmtools_install.sh
@@ -4,7 +4,7 @@
 #scp ~mmovva/VM* root@$i:/tmp 
 cd /tmp
 wget https://www.dropbox.com/s/74j06xkykwjwxfm/VMwareTools-9.4.11-2400950.tar.gz?dl=0
-#tar zxf /tmp/VM*
+tar zxf /tmp/VM*
 cd vmware-tools-distrib
 ./vmware-install.pl -d
 cd /tmp
