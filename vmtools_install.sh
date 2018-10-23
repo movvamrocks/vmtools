9ef59f60746d7b65b4d5bd242d276df0a30d3b69
movvamrocks <movvam@rocketmail.com>
2018-10-23 11:23:30 -0700

    Update vmtools_install.sh

--- vmtools_install.sh
+++ vmtools_install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-
-scp ~mmovva/VM* root@$i:/tmp 
-
-tar zxf /tmp/VM*
+# Unfinished script to quickly complete the vmtools install.
+# Moved archive to public repo like dropbox
+#scp ~mmovva/VM* root@$i:/tmp 
+cd /tmp
+wget https://www.dropbox.com/s/74j06xkykwjwxfm/VMwareTools-9.4.11-2400950.tar.gz?dl=0
+#tar zxf /tmp/VM*
 cd vmware-tools-distrib
 ./vmware-install.pl -d
-
+cd /tmp
+rm -r VM* vm*
 
